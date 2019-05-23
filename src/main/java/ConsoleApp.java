9815dd44ec7d14a2f255774fe7791bf599450eef
Ilya Filisteev <ilya.filisteev@sysqual.net>
2019-05-24 01:41:28 +0300

    * add duplicates writer * add counter for every n-storey houses in each city

--- src/main/java/ConsoleApp.java
+++ src/main/java/ConsoleApp.java
@@ -1,24 +1,59 @@
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 import javax.xml.stream.events.XMLEvent;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stax.StAXResult;
+import javax.xml.transform.stax.StAXSource;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 class ConsoleApp {
+    static class CityFloorKey {
+        private String city;
+        private String floor;
+
+        CityFloorKey(String city, String floor) {
+            this.city = city;
+            this.floor = floor;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj != null && obj instanceof CityFloorKey) {
+                CityFloorKey s = (CityFloorKey) obj;
+                return city.equals(s.city) && floor.equals(s.floor);
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return (city + "." + floor).hashCode();
+        }
+    }
+
     static class ItemKey {
         private String city;
         private String street;
         private String house;
         private String floor;
+
+        ItemKey(String city, String street, String house, String floor) {
+            this.city = city;
+            this.street = street;
+            this.house = house;
+            this.floor = floor;
+        }
 
         @Override
         public boolean equals(Object obj) {
@@ -36,43 +71,67 @@ class ConsoleApp {
     }
 
     //    private static List<String> cityList = new ArrayList<>();
-    private static Map<ItemKey, Integer> cityHashMap = new HashMap<>();
+    private static Map<ItemKey, Integer> itemHashMap = new HashMap<>();
+    private static Map<CityFloorKey, Integer> cityFloorHashMap = new HashMap<>();
 
     static public void main(String[] args) throws XMLStreamException, UnsupportedEncodingException {
         if (args.length == 0) {
             System.err.println("Please specify path to file as first argument.");
         } else {
             processFile(args[0]);
+            /*
+              we can get filtered array with only items which are duplicated,
+              but they are already printed as xml ->
+            */
+            // Map<ItemKey, Integer> duplicates = filterByValue(itemHashMap, x -> (x > 1));
+            System.out.println("How many n-storey buildings in each city:");
+            cityFloorHashMap.forEach((k, v) -> System.out.println("City: " + k.city + " Floors: " + k.floor + " = " + v + " count"));
+
         }
     }
 
-    private static void processFile(String arg) throws XMLStreamException, UnsupportedEncodingException {
-        PrintStream ps = new PrintStream(System.out, true, "cp866");
+    private static void processFile(String arg) throws XMLStreamException {
         File file = new File(arg);
         if (file.exists()) {
-            try (StaxStreamProcessor processor = new StaxStreamProcessor(Files.newInputStream(Paths.get(file.getPath())))) {
+            try (StaxStreamProcessor processor = new StaxStreamProcessor(Files.newInputStream(Paths.get(file.getPath())), System.out)) {
                 XMLStreamReader reader = processor.getReader();
+                XMLStreamWriter writer = processor.getWriter();
+
+                TransformerFactory tf = TransformerFactory.newInstance();
+                Transformer t = tf.newTransformer();
+                StAXSource source = new StAXSource(reader);
+                StAXResult result = new StAXResult(writer);
+
+                System.out.println("Duplicates:");
                 while (reader.hasNext()) {
                     int event = reader.next();
                     if (event == XMLEvent.START_ELEMENT &&
                             "item".equals(reader.getLocalName())) {
 
-                        ItemKey itemKey = new ItemKey();
-                        itemKey.city = reader.getAttributeValue(null, "city");
-                        itemKey.street = reader.getAttributeValue(null, "street");
-                        itemKey.house = reader.getAttributeValue(null, "house");
-                        itemKey.floor = reader.getAttributeValue(null, "floor");
+                        ItemKey itemKey = new ItemKey(
+                                reader.getAttributeValue(null, "city"),
+                                reader.getAttributeValue(null, "street"),
+                                reader.getAttributeValue(null, "house"),
+                                reader.getAttributeValue(null, "floor")
+                        );
 
-                        int count = cityHashMap.getOrDefault(itemKey, 0);
-                        cityHashMap.put(itemKey, ++count);
+                        int count = itemHashMap.getOrDefault(itemKey, 0);
+                        itemHashMap.put(itemKey, ++count);
+
+                        CityFloorKey cityFloorKey = new CityFloorKey(
+                                reader.getAttributeValue(null, "city"),
+                                reader.getAttributeValue(null, "floor")
+                        );
+                        int floorsCount = cityFloorHashMap.getOrDefault(cityFloorKey, 0);
+                        cityFloorHashMap.put(cityFloorKey, ++floorsCount);
+
                         if (count == 2) {
-                            System.out.println(reader.getElementText());
+                            t.transform(source, result);
+                            System.out.println();
                         }
                     }
                 }
-//                Map<ItemKey, Integer> dublicates = filterByValue(cityHashMap, x -> (x > 1));
-                int x = 10;
-            } catch (IOException e) {
+            } catch (IOException | TransformerException e) {
                 e.printStackTrace();
             }
 
--- src/main/java/StaxStreamProcessor.java
+++ src/main/java/StaxStreamProcessor.java
@@ -1,19 +1,25 @@
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.*;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class StaxStreamProcessor implements AutoCloseable {
-    private static final XMLInputFactory FACTORY = XMLInputFactory.newInstance();
+    private static final XMLInputFactory IFACTORY = XMLInputFactory.newInstance();
+    private static final XMLOutputFactory OFACTORY = XMLOutputFactory.newInstance();
 
     private final XMLStreamReader reader;
+    private final XMLStreamWriter writer;
 
-    public StaxStreamProcessor(InputStream is) throws XMLStreamException {
-        reader = FACTORY.createXMLStreamReader(is);
+    public StaxStreamProcessor(InputStream is, OutputStream os) throws XMLStreamException {
+        reader = IFACTORY.createXMLStreamReader(is);
+        writer = OFACTORY.createXMLStreamWriter(os);
     }
 
     public XMLStreamReader getReader() {
         return reader;
+    }
+
+    public XMLStreamWriter getWriter() {
+        return writer;
     }
 
     @Override
