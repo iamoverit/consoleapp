1af15a8f4d80fd7026017b7e6c42bb624b5535bb
Ilya Filisteev <ilya.filisteev@sysqual.net>
2019-05-23 19:25:38 +0300

    Initial commit

---
+++ src/main/java/testconsoleapp.java
@@ -0,0 +1,31 @@
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+class testconsoleapp {
+    static public void main(String[] args) throws IOException, XMLStreamException {
+        File file = new File(args[0]);
+        if(file.exists()){
+            try (StaxStreamProcessor processor = new StaxStreamProcessor(Files.newInputStream(Paths.get(file.getPath())))) {
+                XMLStreamReader reader = processor.getReader();
+                while (reader.hasNext()) {       // while not end of XML
+                    int event = reader.next();   // read next event
+                    if (event == XMLEvent.START_ELEMENT &&
+                            "item".equals(reader.getLocalName())) {
+
+                        System.out.println(reader.getAttributeValue("", "city"));
+                    }
+                }
+            }
+
+        } else {
+            throw new IllegalArgumentException(args[0].concat(" is not valid file resource."));
+        }
+
+    }
+}
+
