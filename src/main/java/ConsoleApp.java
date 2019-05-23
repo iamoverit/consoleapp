c564cbde15916272c2112a31915404d984857bc7
Ilya Filisteev <ilya.filisteev@sysqual.net>
2019-05-23 20:15:17 +0300

    Initial commit

--- src/main/java/ConsoleApp.java
+++ src/main/java/ConsoleApp.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-class testconsoleapp {
+class ConsoleApp {
     static public void main(String[] args) throws IOException, XMLStreamException {
         if (args.length == 0) {
             System.err.println("Please specify path to file as first argument.");
